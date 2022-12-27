3fc04d367535b62dabf95f5ce692a119fda6e7b4
Muhammadyunusxon <Muhammadyunusxonabdumannopov9@gmail.com>
2022-12-27 13:02:29 +0500

    new update

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:pull_to_refresh/pull_to_refresh.dart';
 import 'package:weather/Pages/HomePage.dart';
+
+import 'Style/style.dart';
 
 
 void main() {
@@ -12,16 +15,21 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  ScreenUtilInit(
+    return ScreenUtilInit(
         designSize: const Size(390, 844),
         minTextAdapt: true,
         splitScreenMode: true,
         builder: (context, child) {
-          return  MaterialApp(
-            debugShowCheckedModeBanner: false,
-            title: 'Weather app',
-            home: const HomePage(),
-          );
+          return RefreshConfiguration(
+              headerBuilder: ()=> const WaterDropMaterialHeader(
+                color: Style.primaryColor,
+                backgroundColor: Style.brandColor,
+              ),
+          child: const MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: 'Weather app',
+          home: HomePage(),
+          ));
         });
   }
 }
