9a5b7a99eaa3df746988cf2656e95006cf522b70
Muhammadyunusxon <Muhammadyunusxonabdumannopov9@gmail.com>
2022-12-30 22:01:12 +0500

    update Map

--- lib/Pages/MapPage.dart
+++ lib/Pages/MapPage.dart
@@ -1,6 +1,9 @@
-
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:osm_nominatim/osm_nominatim.dart';
+import 'package:weather/Pages/HomePage.dart';
+import 'package:weather/Repository/get_Information.dart';
+import '../store/local_store.dart';
 
 class MapPage extends StatefulWidget {
   final double lat;
@@ -18,9 +21,40 @@ class _MapPageState extends State<MapPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Google map"),
+        title: const Text("Google map"),
       ),
       body: GoogleMap(
+        mapType: MapType.normal,
+        myLocationEnabled: true,
+        onTap: (location) async {
+          final reverseSearchResult = await Nominatim.reverseSearch(
+              lat: location.latitude,
+              lon: location.longitude,
+              addressDetails: true,
+              extraTags: true,
+              nameDetails: true,
+              language: "en"
+          );
+          print(reverseSearchResult.address);
+          var data = await GetInformationRepository.getInformationWeather(reverseSearchResult.address?["country"]);
+          if (data["error"] == null) {
+            await LocalStore.setCountry(
+                reverseSearchResult.address?["country"]);
+            // ignore: use_build_context_synchronously
+            Navigator.of(context).pushAndRemoveUntil(
+                MaterialPageRoute(builder: (_) => const HomePage()),
+                    (route) => false);
+          } else {
+            // ignore: use_build_context_synchronously
+            ScaffoldMessenger.of(context).showSnackBar(
+              SnackBar(
+                content: Text(
+                  data["error"].toString(),
+                ),
+              ),
+            );
+          }
+        },
         initialCameraPosition:
         CameraPosition(target: LatLng(widget.lat, widget.lon)),
       ),
